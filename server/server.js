8a4b304359c47befa75866d3751d7f54c107383b
Sai Prajnan Emani <sai.emani@citrix.com>
2017-08-16 10:04:44 -0400

    Express added with body parser middle ware. Create Todo post end point added with db

--- server/server.js
+++ server/server.js
@@ -1,3 +1,27 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+
+var app = express();
+
+// body parser middleware
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.status(200).send(doc);
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
