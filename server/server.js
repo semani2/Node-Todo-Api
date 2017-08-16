3f1da28243eaee8ac26ac251c432136820903610
Sai Prajnan Emani <sai.emani@citrix.com>
2017-08-16 12:56:25 -0400

    Setting up for Heroku deployment.

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 // body parser middleware
 app.use(bodyParser.json());
@@ -52,8 +53,8 @@ app.get('/todos/:id', (req, res) => {
     })
 });
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
 
 module.exports = {
