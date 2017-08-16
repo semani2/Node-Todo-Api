42f29ebd8742139f4c9d33eaf69605b9888a3b91
Sai Prajnan Emani <sai.emani@citrix.com>
2017-08-16 11:39:08 -0400

    The GET /todos/:id end point has been implemented.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo');
@@ -31,6 +32,24 @@ app.get('/todos', (req, res) => {
     }, (err) => {
         res.status(400).send(err);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)) {
+        res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(todo) {
+            res.status(200).send(todo);
+        }
+        else {
+            res.status(404).send();
+        }
+    }, (err) => {
+        res.status(400).send();
+    })
 });
 
 app.listen(3000, () => {
