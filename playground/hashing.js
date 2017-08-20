af1fa5d86afc42f4d459967050ddc29e69331f69
Sai Prajnan Emani <sai.emani@citrix.com>
2017-08-19 22:30:02 -0400

    Hash user password before saving implemented.

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,29 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+var password = '123abc!';
 
-var data = {
-    id: 10
-};
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
 
-var token = jwt.sign(data, '123abc');
-console.log(`Token: ${token}`);
+var hashedPassword = '$2a$10$I.Yhnmbn2maetLn6jz.GWetN1xlUW7VhMhNlJjBVVsqJdNPN5ITGa';
 
-var decoded = jwt.verify(token, '123abc');
-console.log(decoded);
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(`Token: ${token}`);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log(decoded);
 
 // var msg = 'I am user number 1';
 
